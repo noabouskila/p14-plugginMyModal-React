d42cdbb9a1ebf29782b80bd5fec5cc922ede1cee
noa bouskila <noabouskil@gmail.com>
2024-08-15 11:52:30 +0200

    essai resolution pb

--- dist/index.js
+++ dist/index.js
@@ -4,22 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _Modal = _interopRequireDefault(require("./Modal"));
+var _Modal = _interopRequireDefault(require("./Modal/Modal"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import App from './App';
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-// Ce fichier sert de point d'entrée principal pour le package npm.
 var _default = exports.default = _Modal.default;
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import Modal  from './Modal/Modal';
+export default Modal;
+
+
